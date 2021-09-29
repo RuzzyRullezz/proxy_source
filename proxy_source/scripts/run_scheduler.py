23730885a10404fd5dff3a760dfc984c637ef014
rgilfanov <rgilfanov@fix.ru>
2021-09-29 13:09:53 +0300

    fixes

--- proxy_source/scripts/run_scheduler.py
+++ proxy_source/scripts/run_scheduler.py
@@ -30,7 +30,7 @@ def add_jobs(scheduler: BaseScheduler):
     )
     scheduler.add_job(
         filter_proxies.filter_proxies,
-        trigger=CronTrigger.from_crontab("*/10 * * * *"),
+        trigger=CronTrigger.from_crontab("*/30 * * * *"),
         id=filter_proxies.filter_proxies.__name__,
         replace_existing=True,
     )
